84e99e696e6b6336b39a2e702089046fcef7b804
doobix <doobix@gmail.com>
2018-04-12 22:17:25 -0700

    use built-in numeric keypad

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Container, Content, Header, Body, Title } from 'native-base';
+import { Container, Content, Header, Body, Title, Form, Item, Label, Input, Button, Text } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+
+import { StyleSheet, View } from 'react-native';
 import KeyPad from './KeyPad';
 
 export default class App extends React.Component {
@@ -19,9 +22,37 @@ export default class App extends React.Component {
           </Body>
         </Header>
         <Content>
-          <KeyPad />
+          <Grid>
+            <Col size={2}>
+              <Form>
+                <Item floatingLabel last>
+                  <Label>Target barbell weight</Label>
+                  <Input keyboardType='numeric' />
+                </Item>
+              </Form>
+            </Col>
+            <Col size={1}>
+              <View style={styles.buttonArea}>
+                <Button primary style={styles.calculateButton}>
+                  <Text>Calculate!</Text>
+                </Button>
+              </View>
+            </Col>
+          </Grid>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonArea: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  calculateButton: {
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+});
