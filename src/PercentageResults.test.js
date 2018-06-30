04fae9500870fe11b13bb14461c2bbea274d6fbc
doobix <doobix@gmail.com>
2018-06-29 19:41:13 -0700

    add tests for percentage results onPress

--- src/PercentageResults.test.js
+++ src/PercentageResults.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PercentageResults, { MAX_PERCENTAGES_MESSAGE, NO_PERCENTAGE_MESSAGE } from './PercentageResults';
 import { shallow } from 'enzyme';
-import { Text } from 'native-base';
+import { CardItem, Text } from 'native-base';
+
+const MOCK_WEIGHT_PERCENTAGES = [
+  { percentage: '100%', weight: '100' },
+  { percentage: '50%', weight: '50' },
+];
 
 const defaultPropsGenerator = (overrides) => ({
+  changeView: jest.fn(),
   isPercentagesCalculated: true,
   calculatedOneRepMaxWeights: [],
+  setWeightAndCalculate: jest.fn(),
   ...overrides,
 });
 
@@ -26,13 +33,32 @@ describe('PercentageResults', () => {
 
   it('renders items from calculatedOneRepMaxWeights', () => {
     const wrapper = setup({
-      calculatedOneRepMaxWeights: [
-        { percentage: '100%', weight: '100' },
-        { percentage: '50%', weight: '50' },
-      ]
+      calculatedOneRepMaxWeights: MOCK_WEIGHT_PERCENTAGES,
     });
     expect(wrapper.find(Text).children().at(0).text()).toBe(MAX_PERCENTAGES_MESSAGE);
     expect(wrapper.find(Text).children().at(1).text()).toBe('@ 100% = 100 lbs');
     expect(wrapper.find(Text).children().at(2).text()).toBe('@ 50% = 50 lbs');
+  });
+
+  it('calls setWeightAndCalculate and changeView functions when result is pressed', () => {
+    const changeView = jest.fn();
+    const setWeightAndCalculate = jest.fn();
+    const wrapper = setup({
+      calculatedOneRepMaxWeights: MOCK_WEIGHT_PERCENTAGES,
+      changeView,
+      setWeightAndCalculate,
+    });
+    expect(changeView).toHaveBeenCalledTimes(0);
+    expect(setWeightAndCalculate).toHaveBeenCalledTimes(0);
+    wrapper.find(CardItem).forEach((item, index) => {
+      if (item.props().button) {
+        item.simulate('press');
+        const weight = MOCK_WEIGHT_PERCENTAGES[index - 1].weight;
+        expect(changeView).toHaveBeenLastCalledWith('plates');
+        expect(setWeightAndCalculate).toHaveBeenLastCalledWith(weight);
+      }
+    });
+    expect(changeView).toHaveBeenCalledTimes(2);
+    expect(setWeightAndCalculate).toHaveBeenCalledTimes(2);
   });
 });
