42cf1e38fdd979acc20a177dc9e063a9c5b69b97
Wesley See <doobix@gmail.com>
2023-09-02 13:36:26 -0700

    fix SettingsView test

--- src/SettingsView.test.js
+++ src/SettingsView.test.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import SettingsView from './SettingsView';
-import { shallow } from 'enzyme';
-import { CheckBox, Input } from 'native-base';
 
 const defaultPropsGenerator = (overrides) => ({
   barbellWeight: '45',
@@ -17,31 +15,34 @@ const defaultPropsGenerator = (overrides) => ({
 });
 
 const setup = (props) => {
-  return shallow(<SettingsView {...defaultPropsGenerator(props)} />);
+  render(<SettingsView {...defaultPropsGenerator(props)} />);
 }
 
 describe('SettingsView', () => {
   it('renders CheckBoxes correctly', () => {
-    const wrapper = setup();
-    expect(wrapper.find(CheckBox)).toHaveLength(3);
-    expect(wrapper.find(CheckBox).at(0).props()).toHaveProperty('checked', false);
-    expect(wrapper.find(CheckBox).at(1).props()).toHaveProperty('checked', true);
-    expect(wrapper.find(CheckBox).at(2).props()).toHaveProperty('checked', false);
+    setup();
+    const checkboxes = screen.getAllByRole('checkbox');
+    expect(checkboxes).toHaveLength(3);
+    expect(checkboxes.at(0).props.accessibilityState.checked).toBeFalsy();
+    expect(checkboxes.at(1).props.accessibilityState.checked).toBeTruthy();
+    expect(checkboxes.at(2).props.accessibilityState.checked).toBeFalsy();
   });
 
-  it('invokes toggleWeightCheckbox when pressing a CheckBox', () => {
+  it('invokes toggleWeightCheckbox when pressing a Checkbox', () => {
     const toggleWeightCheckbox = jest.fn();
-    const wrapper = setup({ toggleWeightCheckbox });
+    setup({ toggleWeightCheckbox });
     expect(toggleWeightCheckbox).not.toHaveBeenCalled();
-    wrapper.find(CheckBox).at(0).simulate('press');
+    const checkboxes = screen.getAllByRole('checkbox');
+    fireEvent(checkboxes.at(0), 'onPress');
     expect(toggleWeightCheckbox).toHaveBeenCalledTimes(1);
   });
 
-  it('invokes onChangeSetBarbellWeight when Input is changed', () => {
-    const onChangeSetBarbellWeight = jest.fn();
-    const wrapper = setup({ onChangeSetBarbellWeight });
-    expect(onChangeSetBarbellWeight).toHaveBeenCalledTimes(0);
-    wrapper.find(Input).simulate('changeText');
-    expect(onChangeSetBarbellWeight).toHaveBeenCalledTimes(1);
+  it('invokes onPressSaveDefaultWeight when save button is pressed', () => {
+    const onPressSaveDefaultWeight = jest.fn();
+    setup({ onPressSaveDefaultWeight });
+    expect(onPressSaveDefaultWeight).toHaveBeenCalledTimes(0);
+    const button = screen.getByRole('button');
+    fireEvent(button, 'onPress');
+    expect(onPressSaveDefaultWeight).toHaveBeenCalledTimes(1);
   });
 });
