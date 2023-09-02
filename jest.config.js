ee5df2d1a769a3ec8fbb235b84e98318873b3dc7
Wesley See <doobix@gmail.com>
2023-09-02 13:36:01 -0700

    update babel/jest/packages

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // preset: 'react-native',
+  preset: 'jest-expo',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom',
+    '@testing-library/jest-native',
+  ],
+  setupFiles: [
+    '<rootDir>/jest-setup.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-vector-icons)',
+  ],
+};
