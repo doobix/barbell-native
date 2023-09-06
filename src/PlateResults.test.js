c7bb5f5d5c17a9f40867bea511b146400e0cad46
Wesley See <doobix@gmail.com>
2023-09-06 10:52:23 -0700

    fix PlateResults test

--- src/PlateResults.test.js
+++ src/PlateResults.test.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import PlateResults, { ADD_WEIGHT_MESSAGE, LEFTOVER_WEIGHT_MESSAGE, NO_WEIGHT_MESSAGE } from './PlateResults';
-import { shallow } from 'enzyme';
-import { Text } from 'native-base';
 
 const defaultPropsGenerator = (overrides) => ({
   calculatedWeight: 0,
@@ -12,24 +10,24 @@ const defaultPropsGenerator = (overrides) => ({
 });
 
 const setup = (props) => {
-  return shallow(<PlateResults {...defaultPropsGenerator(props)} />);
+  render(<PlateResults {...defaultPropsGenerator(props)} />);
 }
 
 describe('PlateResults', () => {
   it('renders nothing when isWeightsCalculated is false', () => {
-    const wrapper = setup({ isWeightsCalculated: false });
-    expect(wrapper.html()).toBeNull();
+    setup({ isWeightsCalculated: false });
+    expect(screen.root).toBeUndefined();
   });
 
   it('renders message when calculatedWeights is empty', () => {
-    const wrapper = setup();
-    expect(wrapper.find(Text).children().text()).toBe(NO_WEIGHT_MESSAGE);
+    setup();
+    expect(screen.getAllByText(NO_WEIGHT_MESSAGE)).toHaveLength(1);
   });
 
   it('renders message when there are leftover weights', () => {
     const calculatedWeight = 51;
     const leftoverWeight = 1;
-    const wrapper = setup({
+    setup({
       calculatedWeight,
       calculatedWeights: [
         { count: '1', weight: '2.5' },
@@ -39,18 +37,18 @@ describe('PlateResults', () => {
     const leftoverMessage = LEFTOVER_WEIGHT_MESSAGE
       .replace('{leftoverWeight}', leftoverWeight)
       .replace('{calculatedWeight}', calculatedWeight)
-    expect(wrapper.find(Text).children().at(0).text()).toBe(leftoverMessage);
+    expect(screen.getAllByText(leftoverMessage)).toHaveLength(1);
   });
 
   it('renders items from calculatedWeights', () => {
-    const wrapper = setup({
+    setup({
       calculatedWeights: [
         { count: '2', weight: '45' },
         { count: '1', weight: '2.5' },
       ]
     });
-    expect(wrapper.find(Text).children().at(0).text()).toBe(ADD_WEIGHT_MESSAGE);
-    expect(wrapper.find(Text).children().at(1).text()).toBe('2 x 45 lbs');
-    expect(wrapper.find(Text).children().at(2).text()).toBe('1 x 2.5 lbs');
+    expect(screen.getAllByText(ADD_WEIGHT_MESSAGE)).toHaveLength(1);
+    expect(screen.getAllByText('2 x 45 lbs')).toHaveLength(1);
+    expect(screen.getAllByText('1 x 2.5 lbs')).toHaveLength(1);
   });
 });
